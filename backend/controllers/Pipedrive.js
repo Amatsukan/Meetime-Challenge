02dfb8e6d123fdb483d0077da783e7b2707cdc53
Luiz jr <luiz.cfj@apilab.ufsc.br>
2018-03-20 22:06:25 -0300

    improve pipedrive token validation

--- backend/controllers/Pipedrive.js
+++ backend/controllers/Pipedrive.js
@@ -24,8 +24,12 @@ module.exports = {
     postKey : function(req, res){
         //req.body.key = "6bfe3419d960e014c55a637083114cdf177e3da0" minha chave
 
+        if( req.body.key == undefined ){
+            res.status(400).send({error: 'Invalid key'})
+            return;
+        }
+
         fetch( 'https://companydomain.pipedrive.com/v1/persons?api_token='+req.body.key ).then( response => response.json().then( json => {
-                console.log(json)
                 if(json.success == true){
                     pipedrive_con = new Pipedrive.Client( req.body.key, { strictMode: true } );
                     res.status(200).send({status: "ok"})
